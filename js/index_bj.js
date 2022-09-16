228a7871bf969ec460a2883c61d93e8b98cb24ba
Marcusyyds <366959409@qq.com>
2022-09-16 15:36:32 +0800

    Site updated: 2022-09-16 15:36:25

---
+++ js/index_bj.js
@@ -0,0 +1,11 @@
+var now = new Date();
+var hour = now.getHours();
+if (hour >= 7 && hour < 18){
+document.write("<style>background-image: url('https://bu.dusays.com/2022/09/16/63242685d8c4d.jpg')</style>");
+}
+if (hour >= 18 && hour < 19){
+document.write("<style>background-image: url('https://bu.dusays.com/2022/09/16/6323d377528a3.jpg')</style>");
+}
+if (hour >= 19 && hour <7){
+document.write("<style>background-image: url('https://bu.dusays.com/2022/09/16/6323d377528a3.jpg')</style>");
+}
