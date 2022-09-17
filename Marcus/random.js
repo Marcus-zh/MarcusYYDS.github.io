2f45dd0e7de27b5ef016c4f0adda0a08990057f8
Marcusyyds <366959409@qq.com>
2022-09-17 08:03:57 +0800

    Site updated: 2022-09-17 08:03:55

--- Marcus/random.js
+++ Marcus/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/e70bf4f.html","posts/6709fc4a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/e70bf4f.html","posts/6709fc4a.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
